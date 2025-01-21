50e77c5c079d88bbfeefccfacf61c294541dd311
Maxime Rouyez <maxirouy522@gmail.com>
2025-01-21 16:41:09 +0100

    Mouahahaha

--- Python-avancé/messenger.py
+++ Python-avancé/messenger.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 from argparse import ArgumentParser
 
-from model import User
-from model import Channels
-from model import Messages
-from server import Server
+from remoteserver import Remoteserver
+from localserver import Localserver
 from client import Client
 
 class RemoteServer:
@@ -16,15 +14,14 @@ argument_parser.add_argument('-f', '--filename')
 argument_parser.add_argument('-u', '--url')
 argument_parser.add_argument('-p', '--portail', action = 'store_true')
 arguments = argument_parser.parse_args()
-server: Server
 if arguments.filename is not None:
-    serveur = Server(arguments.filename)
+    server = Localserver(arguments.filename)
 elif arguments.url is not None:
-    serveur = RemoteServer(arguments.url)
+    server = RemoteServer(arguments.url)
 else:
     print('Error: -f or -u should be set')
     exit(-1)
 
-server = Server.load_from_json_file(arguments.jsonfile) 
+server.load_server()
 client = Client(server) 
 client.accueil()

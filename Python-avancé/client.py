30fd7c4bf2ed2fe7ab5a8a52ded8cf374995ecf5
Maxime Rouyez <maxirouy522@gmail.com>
2025-01-21 16:06:20 +0100

    mouahahaha

--- Python-avancé/client.py
+++ Python-avancé/client.py
@@ -1,6 +1,5 @@
 from model import User
 from model import Channels
-from model import Messages
 from server import Server
 
 class Client:
@@ -15,13 +14,13 @@ class Client:
         if choice == 'x':
             print('Bye!')
         elif choice == '1':
-            Client.userscreen()
+            self.userscreen()
         elif choice == '2':
-            Client.channelscreen()
+            self.channelscreen()
 
     def userscreen (self):
         print('Users:')
-        server_with_classes = Server.load_server()
+        server_with_classes = self.server.load_server()
         users: list[User] = server_with_classes['users']
         for user in users:
             print(user.id, '.', user.name)
@@ -29,15 +28,15 @@ class Client:
         print('x. Main Menu')
         choice = input('Select an option and press <Enter>: ')
         if choice == 'x':
-            Client.start()
+            self.start()
         elif choice == 'n':
-            Client.newuser()
+            self.newuser()
         else:
-            Client.userscreen()
+            self.userscreen()
 
     def channelscreen (self):
         print('channels')
-        server_with_classes = Server.load_server()
+        server_with_classes = self.server.load_server()
         channels = server_with_classes['channels']
         for channel in channels:
             print(channel.id, '.', channel.name)
@@ -45,14 +44,14 @@ class Client:
         print('n. Create channel')
         choice = input('Select an option and press <Enter>: ')
         if choice == 'x':
-            Client.start()
+            self.start()
         elif choice == 'n':
-            Client.newchannel()
+            self.newchannel()
         else: 
-            Client.channelscreen()
+            self.channelscreen()
 
     def newchannel(self):
-        server_with_classes = Server.load_server()
+        server_with_classes = self.server.load_server()
         nomgrp = input('Select a name and press <Enter>: ')
         n_id = max(channel.id for channel in server_with_classes['channels']) + 1
         L=[]
@@ -72,21 +71,21 @@ class Client:
                     L.append(n2_id)
             new_channel = Channels(n2_id, nomgrp, L)
             server_with_classes['channels'].append(new_channel)
-            Server.save_server(server_with_classes)
+            self.server.save_server(server_with_classes)
         elif choice == 'x':
-            Client.channelscreen()
+            self.channelscreen()
         else:
-            Client.channelscreen()
-        Client.accueil()
+            self.channelscreen()
+        self.accueil()
 
     def newuser(self):
-        server_with_classes = Server.load_server()
+        server_with_classes = self.server.load_server()
         nom = input('Select a name and press <Enter>: ')
         n_id = max(user.id for user in server_with_classes['users']) + 1
         new_user = User(nom, n_id)
         server_with_classes['users'].append(new_user)
-        Server.save_server(server_with_classes)
-        Client.userscreen()
+        self.server.save_server(server_with_classes)
+        self.userscreen()
 
     def accueil(self): 
         print('=== Messenger ===')
@@ -97,9 +96,9 @@ class Client:
         if choice == 'x':
             print('Bye!')
         elif choice== '1':
-            Client.userscreen()
+            self.userscreen()
         elif choice == '2':
-            Client.channelscreen()
+            self.channelscreen()
         else :
             print('Unknown option:', choice)
-            Client.accueil()
+            self.accueil()
